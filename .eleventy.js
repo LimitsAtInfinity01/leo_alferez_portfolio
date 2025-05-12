3b102f88a1268bc9a197e7f050a3a3f74a3d0993
LimitsAtInfinity01 <mmddcky@hotmail.com>
2025-05-11 22:23:38 -0600

    added config for github pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,18 @@
 module.exports = function(eleventyConfig) {
-
+  // Passthroughs
   eleventyConfig.addPassthroughCopy("src/styles");
-  eleventyConfig.addPassthroughCopy('src/assets');
+  eleventyConfig.addPassthroughCopy("src/assets");
 
-    return {
-      dir: {
-        input: "src",       // Source folder
-        includes: "_includes", // Folder for layouts and includes
-        output: "_site"     // Output folder
-      }
-    };
+  // Return 11ty config
+  return {
+    // Directory structure
+    dir: {
+      input: "src",            // Source folder
+      includes: "_includes",   // Layouts/includes folder
+      output: "_site"          // Build output
+    },
+
+    // Base path for URLs (project pages)
+    pathPrefix: "/<repo>/"     // ← Move this out here
   };
-  
+};
